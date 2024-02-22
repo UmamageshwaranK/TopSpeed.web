b6c92a3ee48ec01fa1bb03da43fc0e39bc90df2e
UmamageshwaranK <umamageshwarank6@gmail.com>
2024-02-22 18:27:19 +0530

    DbContext set

---
+++ TopSpeed.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TopSpeed.Web.Data
+{
+    public class ApplicationDbContext : DbContext   // Its using Entity FrameWork Core Library
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+            {
+
+
+            }
+
+    }
+}
---
+++ TopSpeed.Web/Models/Brand.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace TopSpeed.Web.Models
+{
+    public class Brand
+    {
+        [Key]   // Its an primary Key    Its Used the DataAnnotation library
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set;}
+
+        public int Established { get; set; }
+
+        public string BrandLogo { get; set;  }
+
+    }
+}
